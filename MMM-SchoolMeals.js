dda2d7fa9d385b086f5ca4cab81496b1ec7cefc5
kkwjk2718 <158192089+kkwjk2718@users.noreply.github.com>
2024-01-31 02:43:19 +0900

    Add files via upload

---
+++ MMM-SchoolMeals.js
@@ -0,0 +1,81 @@
+Module.register("MMM-SchoolMeals", {
+    defaults: {
+    },
+
+    start: function() {
+        this.meals = [];
+        this.getMeals();
+
+        // 매일 자정에 급식 정보 업데이트
+        var self = this;
+        var refreshSchedule = function() {
+            var now = new Date();
+            var millisTillMidnight = new Date(now.getFullYear(), now.getMonth(), now.getDate(), 24, 0, 0, 0) - now;
+            if (millisTillMidnight < 0) {
+                millisTillMidnight += 86400000; 
+            }
+            setTimeout(function() {
+                self.getMeals();
+                refreshSchedule(); 
+            }, millisTillMidnight);
+        };
+
+        refreshSchedule();
+    },
+
+    getMeals: function() {
+        this.sendSocketNotification("GET_MEALS", this.config);
+    },
+
+    getScripts: function() {
+        return [];
+    },
+
+    getStyles: function() {
+        return [];
+    },
+
+    getDom: function() {
+        var wrapper = document.createElement("table");
+        wrapper.className = "small table";
+    
+        var headerRow = document.createElement("tr");
+        headerRow.innerHTML = "<th>조식</th><th>중식</th><th>석식</th>";
+        wrapper.appendChild(headerRow);
+    
+        var mealsRow = document.createElement("tr");
+        var caloriesRow = document.createElement("tr");
+        var mealsTypes = { "조식": "급식 없음", "중식": "급식 없음", "석식": "급식 없음" };
+        var caloriesTypes = { "조식": "", "중식": "", "석식": "" };
+    
+        this.meals.forEach(meal => {
+            var mealName = meal.MMEAL_SC_NM; 
+            var mealInfo = meal.DDISH_NM.trim() === "" ? "급식 없음" : meal.DDISH_NM.replace(/\([^)]*\)/g, '').replace(/\s+/g, ', ');
+            var calInfo = meal.CAL_INFO || "";
+            mealsTypes[mealName] = mealInfo;
+            caloriesTypes[mealName] = calInfo;
+        });
+    
+        
+        mealsRow.innerHTML += `<td>${mealsTypes["조식"]}</td>`;
+        mealsRow.innerHTML += `<td>${mealsTypes["중식"]}</td>`;
+        mealsRow.innerHTML += `<td>${mealsTypes["석식"]}</td>`;
+    
+        caloriesRow.innerHTML += `<td>${caloriesTypes["조식"]}</td>`;
+        caloriesRow.innerHTML += `<td>${caloriesTypes["중식"]}</td>`;
+        caloriesRow.innerHTML += `<td>${caloriesTypes["석식"]}</td>`;
+    
+        wrapper.appendChild(mealsRow);
+        wrapper.appendChild(caloriesRow);
+    
+        return wrapper;
+    }
+,    
+
+    socketNotificationReceived: function(notification, payload) {
+        if (notification === "MEALS_RESULT") {
+            this.meals = payload;
+            this.updateDom();
+        }
+    }
+});
